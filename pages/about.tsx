abb4e2da0b76f85fb2cb9c5b4096702e8330d593
Pedrozle <pedrozle@outlook.com>
2023-04-08 18:59:39 -0400

    adicionado adicionado meta tags

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,7 +1,12 @@
-export default function About(){
+import Head from "next/head";
+
+export default function About() {
     return (
         <>
+            <Head>
+                <title>Sobre Nós</title>
+            </Head>
             <h1>Página de Sobre</h1>
         </>
-    )
+    );
 }
--- pages/products/index.tsx
+++ pages/products/index.tsx
@@ -1,6 +1,11 @@
+import Head from "next/head";
+
 export default function Products() {
     return (
         <>
+            <Head>
+                <title>Produtos</title>
+            </Head>
             <h1>Página de Produtos</h1>
         </>
     );
