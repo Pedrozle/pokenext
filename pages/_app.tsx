a1705af5d70b975332395e9e3bacb97fdf2964c5
Pedrozle <pedrozle@outlook.com>
2023-04-08 20:58:53 -0400

    rework

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
-import MainContainer from "@/components/mainContainer";
+import MainContainer from "@/components/MainContainer";
 
 export default function App({ Component, pageProps }: AppProps) {
     return (
