f46326c35d07cf964141f60373f43f16e830f3b4
Pedrozle <pedrozle@outlook.com>
2023-04-08 20:16:28 -0400

    criado rotas dinamicas

--- pages/todos/[todoId].tsx
+++ pages/todos/[todoId].tsx
@@ -1,20 +1,46 @@
 import Link from "next/link";
 
-import { useRouter } from "next/router";
+export async function getStaticProps(context: any) {
+    const { params } = context;
+    const data = await fetch(
+        `https://jsonplaceholder.typicode.com/todos/${params.todoId}`
+    );
 
-export default function Todo() {
-    const router = useRouter();
+    const todo = await data.json();
 
-    const todoId = router.query.todoId;
+    return {
+        props: { todo },
+    };
+}
 
+export async function getStaticPaths() {
+    const response = await fetch("https://jsonplaceholder.typicode.com/todos/");
+
+    const data = await response.json();
+
+    const paths = data.map((todo: any) => {
+        return {
+            params: {
+                todoId: `${todo.id}`,
+            },
+        };
+    });
+
+    return { paths, fallback: false };
+}
+
+export default function Todo({ todo }: any) {
     return (
         <>
             <Link href="/todos">Voltar</Link>
-            <h1>Exibindo o Todo: {todoId} </h1>
+            <h1>Exibindo o Todo: {todo.id} </h1>
+            <h2>Titulo: {todo.title}</h2>
+            <h3>Usuario: {todo.userId}</h3>
             <p>
                 Comentario: la la la{" "}
-                <Link href={`/todos/${todoId}/comments/1`}>Detalhes</Link>
+                <Link href={`/todos/${todo.id}/comments/1`}>Detalhes</Link>
             </p>
+            <p>status: {todo.completed ? "yes" : "no"}</p>
         </>
     );
 }
--- pages/todos/index.tsx
+++ pages/todos/index.tsx
@@ -1,11 +1,10 @@
 import styles from "@/styles/pages/todo.module.css";
+import Link from "next/link";
 
 export async function getStaticProps() {
     const resp = await fetch("https://jsonplaceholder.typicode.com/todos");
 
     const data = await resp.json();
-
-    console.log({ props: { data } });
 
     return { props: { data } };
 }
@@ -16,7 +15,10 @@ export default function Todos({ data }: any) {
             <h1>Tarefas para fazer:</h1>
             <ul className={styles.todolist}>
                 {data.map((prop: any) => (
-                    <li key={prop.id}>{prop.title}</li>
+                    <li key={prop.id}>
+                        {prop.title} -{" "}
+                        <Link href={`/todos/${prop.id}`}>Detalhes</Link>
+                    </li>
                 ))}
             </ul>
         </>
